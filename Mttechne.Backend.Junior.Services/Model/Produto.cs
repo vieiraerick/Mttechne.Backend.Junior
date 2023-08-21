9411ba3f005976f9efc1e3356ab65facc1123a1b
GuilhermeNicolodi <gui.nicolodi@hotmail.com>
2023-08-21 00:26:48 -0300

    GuilhermeNicolodi => Finalizacao do teste

--- Mttechne.Backend.Junior.Services/Model/Produto.cs
+++ Mttechne.Backend.Junior.Services/Model/Produto.cs
@@ -1,7 +1,10 @@
-﻿namespace Mttechne.Backend.Junior.Services.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mttechne.Backend.Junior.Services.Model;
 
 public class Produto
 {
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
     public string Nome { get; set; } = null!;
     public int Valor { get; set; }
 }
--- Mttechne.Backend.Junior.Services/Services/IProdutoService.cs
+++ Mttechne.Backend.Junior.Services/Services/IProdutoService.cs
@@ -5,5 +5,10 @@ namespace Mttechne.Backend.Junior.Services.Services;
 public interface IProdutoService
 {
     List<Produto> GetListaProdutos();
-    List<Produto> GetListaProdutosPorNome(string nome);
+    List<Produto> GetListaProdutosByName(string nome);
+    List<Produto> GetListaProdutosByPriceAscendinge();
+    List<Produto> GetListaProdutosByPriceDescending();
+    List<Produto> GetListaProdutosInPriceRange(int minPrice, int maxPrice);
+    List<Produto> GetListaProdutosPerMaxValue();
+    List<Produto> GetListaProdutosPerMinValue();
 }
