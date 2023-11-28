7847530fa9fea7d7896476698e924e347e326866
Felipe Henrique Astolfi Bazon <felipeastolfi@gmail.com>
2023-11-28 11:07:22 -0300

    Adicionados testes e adaptado enpoints com tratamentos de erros

--- Mttechne.Backend.Junior.Interface/Controllers/ProdutoController.cs
+++ Mttechne.Backend.Junior.Interface/Controllers/ProdutoController.cs
@@ -15,22 +15,86 @@ public class ProdutoController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<IActionResult> GetListaProdutos() => Ok(_service.GetListaProdutos());
+    public async Task<IActionResult> GetListaProdutos()
+    {
+        try
+        {
+            return Ok(_service.GetListaProdutos());
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
 
     [HttpGet("{nome}")]
-    public async Task<IActionResult> GetListaProdutosProNome([FromRoute] string nome) => Ok(_service.GetListaProdutosPorNome(nome));
+    public async Task<IActionResult> GetListaProdutosPorNome([FromRoute] string nome)
+    {
+        try
+        {
+            var data = _service.GetListaProdutosPorNome(nome);
+
+            if (data.Count() == 0)
+                return NoContent();
+
+            return Ok(data);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
 
     [HttpGet("BuscaProdutoOrderByValor/{order}")]
-    public async Task<IActionResult> GetListaProdutosOrdenada([FromRoute] string order) => order.ToLower().Equals("asc") ? Ok(_service.GetListaProdutos().OrderBy(x => x.Valor)) : Ok(_service.GetListaProdutos().OrderByDescending(x => x.Valor));
-    [HttpGet("BuscaProdutoEntreValores/{valorInicial}/{valorFinal}")]
-    public async Task<IActionResult> GetListaProdutosOrdenada([FromRoute] double valorInicial, double valorFinal)
+    public async Task<IActionResult> GetListaProdutosOrdenada([FromRoute] string order)
     {
-        if (valorFinal <= valorInicial)
-            return BadRequest("Os valores do filtro estão inválidos");
-        return Ok(_service.GetListaProdutosPorIntervaloValores(valorInicial,valorFinal));
+        try
+        {
+            return order.ToLower().Equals("asc") ? Ok(_service.GetListaProdutos().OrderBy(x => x.Valor)) : Ok(_service.GetListaProdutos().OrderByDescending(x => x.Valor));
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpGet("BuscaProdutoEntreValores/{valorInicial}/{valorFinal}")]
+    public async Task<IActionResult> GetListaProdutosEntreValores([FromRoute] double valorInicial, double valorFinal)
+    {
+        try
+        {
+            if (valorFinal <= valorInicial)
+                return Conflict("Os valores do filtro estão inválidos");
+            return Ok(_service.GetListaProdutosPorIntervaloValores(valorInicial, valorFinal));
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
     }
     [HttpGet("BuscaProdutoValorMaximo")]
-    public async Task<IActionResult> GetListaProdutosPorValorMaximo() => Ok(_service.GetListaProdutosPorValorMaximo());
+    public async Task<IActionResult> GetListaProdutosPorValorMaximo()
+    {
+        try
+        {
+            return Ok(_service.GetListaProdutosPorValorMaximo());
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
     [HttpGet("BuscaProdutoValorMinimo")]
-    public async Task<IActionResult> GetListaProdutosPorValorMinimo() => Ok(_service.GetListaProdutosPorValorMinimo());
+    public async Task<IActionResult> GetListaProdutosPorValorMinimo()
+    {
+        try
+        {
+            return Ok(_service.GetListaProdutosPorValorMinimo());
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
 }
