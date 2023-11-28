fe13a89e70090e9b4db3874c494c95fc0ea0486d
Felipe Henrique Astolfi Bazon <felipeastolfi@gmail.com>
2023-11-28 09:59:47 -0300

    Adicionado EntityFrameworkCore e Pomelo, adicionado relacionamento com banco de dados MySQL

---
+++ Mttechne.Backend.Junior.Services/Context/AppDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mttechne.Backend.Junior.Services.Model;
+
+namespace Mttechne.Backend.Junior.Services.Context
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>().HasData(
+            new Produto() { ProdutoId = 1, Nome = "Placa de Vídeo", Valor = 1000 },
+            new Produto() { ProdutoId = 2, Nome = "Placa de Vídeo", Valor = 1500 },
+            new Produto() { ProdutoId = 3, Nome = "Placa de Vídeo", Valor = 1350 },
+            new Produto() { ProdutoId = 4, Nome = "Processador", Valor = 2000 },
+            new Produto() { ProdutoId = 5, Nome = "Processador", Valor = 2100 },
+            new Produto() { ProdutoId = 6, Nome = "Memória", Valor = 300 },
+            new Produto() { ProdutoId = 7, Nome = "Memória", Valor = 350 },
+            new Produto() { ProdutoId = 8, Nome = "Placa mãe", Valor = 1100 });
+        }
+    }
+}
--- Mttechne.Backend.Junior.Services/Services/ProdutoService.cs
+++ Mttechne.Backend.Junior.Services/Services/ProdutoService.cs
@@ -1,25 +1,16 @@
-﻿using Mttechne.Backend.Junior.Services.Model;
+﻿using Mttechne.Backend.Junior.Services.Context;
+using Mttechne.Backend.Junior.Services.Model;
 
 namespace Mttechne.Backend.Junior.Services.Services;
 
 public class ProdutoService : IProdutoService
 {
+    private AppDBContext _ctx;
+    public ProdutoService(AppDBContext ctx) => _ctx = ctx;
     public List<Produto> GetListaProdutos()
     {
-        Produto produto1 = new Produto() { Nome = "Placa de Vídeo", Valor = 1000 };
-        Produto produto2 = new Produto() { Nome = "Placa de Vídeo", Valor = 1500 };
-        Produto produto3 = new Produto() { Nome = "Placa de Vídeo", Valor = 1350 };
-        Produto produto4 = new Produto() { Nome = "Processador", Valor = 2000 };
-        Produto produto5 = new Produto() { Nome = "Processador", Valor = 2100 };
-        Produto produto6 = new Produto() { Nome = "Memória", Valor = 300 };
-        Produto produto7 = new Produto() { Nome = "Memória", Valor = 350 };
-        Produto produto8 = new Produto() { Nome = "Placa mãe", Valor = 1100 };
-        
-        List<Produto> produtosCadastrados = new List<Produto>()
-        {
-            produto1, produto2, produto3, produto4, produto5, produto6, produto7, produto8
-        };
-        
+        List<Produto> produtosCadastrados = _ctx.Produtos.ToList();
+
         return produtosCadastrados;
     }
 
@@ -41,7 +32,7 @@ public class ProdutoService : IProdutoService
     {
         var listaProdutos = GetListaProdutos();
 
-        return listaProdutos.OrderByDescending(x => x.Valor).DistinctBy(x=>x.Nome).ToList();
+        return listaProdutos.OrderByDescending(x => x.Valor).DistinctBy(x => x.Nome).ToList();
     }
 
     public List<Produto> GetListaProdutosPorValorMinimo()
